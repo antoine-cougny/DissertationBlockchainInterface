debbf29b67d1277887684b2bc98ed0ebc745c78d
Antoine <aoc2@hw.ac.uk>
2018-06-29 17:08:26 +0100

    added taskToken class structure

---
+++ contracts/taskToken.sol
@@ -0,0 +1,9 @@
+// Task Token
+pragma solidity ^0.4.23
+
+import "zeppelin-solidity/contracts/token/ERC721/ERC721BasicToken.sol"
+import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol"
+
+contract taskToken is ERC721BasicToken {
+
+}
