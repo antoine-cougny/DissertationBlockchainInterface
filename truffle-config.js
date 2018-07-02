96cd23f9a97003fbb7343a9afc8355b6ef6e1313
Antoine <aoc2@hw.ac.uk>
2018-07-02 15:18:46 +0100

    config to deploy contract to local test network

--- truffle-config.js
+++ truffle-config.js
@@ -14,5 +14,18 @@
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  // to customize your Truffle configuration
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      gas: 3500000
+    },
+    ourTestNet: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" 
+    }
+  }
 };
