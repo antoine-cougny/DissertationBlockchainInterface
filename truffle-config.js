3253e4a60d770227941193111660a62932eabc05
Antoine <aoc2@hw.ac.uk>
2018-07-05 16:25:47 +0100

    added babel config from cryptokitties clone tutp

--- truffle-config.js
+++ truffle-config.js
@@ -12,6 +12,11 @@
  *   },
  */
 
+require("babel-register")({
+  ignore: /node_modules\/(?!zeppelin-solidity)/
+});
+require("babel-polyfill");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration
